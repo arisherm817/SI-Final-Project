a3a32c1b5a6d4bb28341d17bdbfec710e0387488
arisherm <arisherm@umich.edu>
2021-04-21 21:39:18 -0400

    set up vaccine tables

--- gathering-vaccine-data.py
+++ gathering-vaccine-data.py
@@ -9,11 +9,11 @@ import csv
 #Covid-19 Cases and Vaccines
 #Team members: Lindsay Brenner and Ari Sherman 
 
-# def set_up_database(name):
-#     path = os.path.dirname(os.path.abspath(__file__))
-#     conn = sqlite3.connect(path + '/' + name)
-#     cur = conn.cursor()
-#     return cur, conn
+def set_up_database(name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + '/' + name)
+    cur = conn.cursor()
+    return cur, conn
 
 def get_vaccine_data():
     data = {}
@@ -23,44 +23,79 @@ def get_vaccine_data():
     divs = soup.find('div', id = 'covid19-container')
     table = divs.find('table')
     trs = table.find_all('tr')
-    for i in trs[3:]: 
+    for i in trs[2:193]: 
         tds = i.find_all('td')
         td = tds[0]
         name = td.find('a')
         country = name.text.strip()
         number = tds[2]
         percent = number.text.strip('%')
-        data[country] = percent
-    print(data)
-
-    # for i in trs: 
-    #     country = i.find('td', class_ = 'entity sorted')
-    #     name = country.text.strip()
-    #     percent_row = i.find('td', class_ = 'dimension dimension-end')
-    #     percent = int(percent_row.text.strip('%'))
-    #     data[name] = percent
-    # print(data)
-    #d = soup.find_all('div', class_ = 'covid19-container')
+        number = (tds[1].text.replace(',', ''))
+        if percent == '--':
+            continue
+        else:
+            
+            data[country] = (float(percent), int(number))
+    return data
     
 
-def fill_table(cur, conn): 
-    pass
+def fill_country_id_table(cur, conn): 
+    cur.execute("CREATE TABLE IF NOT EXISTS CountryIds (country_id INTEGER PRIMARY KEY, country TEXT)")
+    countries = get_vaccine_data()
+    countries_list = []
 
-def set_up_vaccine_table(cur, conn):
+    while len(countries_list) < len(countries):
+        count = 0
+        for country in countries:
+                country_id = len(countries_list) + 1
+                if country in countries_list:
+                    continue
+                else:
+                    cur.execute("INSERT OR IGNORE INTO CountryIds (country_id, country) VALUES (?,?)", (country_id, country))
+                    count += 1
+                    countries_list.append(country)
+                if count == 25:
+                    break
+    conn.commit()
+    
+def fill_vaccine_table(cur, conn): 
+    cur.execute("CREATE TABLE IF NOT EXISTS VaccineTable (country_id INTEGER PRIMARY KEY, vaccinated INTEGER, percent NUMBER)")
+    countries = get_vaccine_data()
+    countries_list = []
+
+    while len(countries_list) < len(countries):
+        count = 0
+        for country in countries:
+                country_id = len(countries_list) + 1
+                if country in countries_list:
+                    continue
+                else:
+                    cur.execute("INSERT OR IGNORE INTO VaccineTable (country_id, vaccinated, percent) VALUES (?,?,?)", (country_id, countries[country][1], countries[country][0]))
+                    count += 1
+                    countries_list.append(country)
+                if count == 25:
+                    break
+    conn.commit()
+
+
+def set_up_vaccine_tables(cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS CountryIds (country_id INTEGER PRIMARY KEY, country TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS VaccineTable (country_id INTEGER PRIMARY KEY, vaccinated INTEGER, percent NUMBER)")
+    conn.commit()
+
+def calculate():
     pass
 
 def write_data_file(filename, cur, conn):
     pass
 
 def main():
-    get_vaccine_data()
+    cur, conn = set_up_database('vaccine.db')
+    set_up_vaccine_tables(cur, conn)
+    fill_country_id_table(cur, conn)
+    fill_vaccine_table(cur,conn)
 
-    # cur, conn = set_up_database('vaccine.db')
-    # set_up_vaccine_table(cur, conn)
-    # fill_table(cur, conn)
-    
-    # write_data_file("vaccine-data.txt", cur, conn)
-    # conn.close()
+    conn.close()
 
 
 if __name__ == "__main__":
